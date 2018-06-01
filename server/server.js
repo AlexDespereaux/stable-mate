258077dce675d04fa70cf23d975553d0ccb807f7
Richard Morton <richard.james.morton@gmail.com>
2018-06-01 22:27:38 +1000

    pulled out api logic to seperate file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 'use strict';
 
 const express = require('express');
-var path = require('path');
+const path = require('path');
+const api = require('./api');
 
-// Constants
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// app.use('/api', api);
+app.use('/api', api);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
