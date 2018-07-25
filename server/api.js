58c5a8bdd228b22acb50466318a0e3c3c9be7e7e
Richard Morton <richard.james.morton@gmail.com>
2018-07-25 16:01:34 +1000

    s3 bucketing

--- server/api.js
+++ server/api.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const AWS = require('aws-sdk');
+const stream = require('stream');
+
 const router = express.Router();
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const BUCKET = 'annomate';
 
 let printRequestHeaders = function(req) {
-    console.log("\nReceived headers");
-    console.log("----------------");
+  console.log("\nReceived headers");
+  console.log("----------------");
 
-    for (let key in req.headers) {
-        console.log(key + ": " + req.headers[key]);
-    }
+  for (let key in req.headers) {
+    console.log(key + ": " + req.headers[key]);
+  }
 
-    console.log("");
+  console.log("");
 };
 
-let binaryUploadHandler = function(req, res) {
-    console.log("\n\nBinary Upload Request from: " + req.ip);
-    printRequestHeaders(req);
+let uploadHandler = function(req, res) {
+  console.log("\n\nBinary Upload Request from: " + req.ip);
+  printRequestHeaders(req);
 
-  let s3 = new AWS.S3({apiVersion: '2006-03-01'});
-    let filename = req.headers["file-name"];
-    console.log("Started binary upload of: " + filename);
-    req.pipe(uploadFromStream(s3));
-    req.on('end', function() {
-        res.sendStatus(200);
-    });
+
+  let filename = req.headers["file-name"];
+  console.log("Started binary upload of: " + filename);
+  req.pipe(uploadFromStream(s3));
+  req.on('end', function() {
+    res.sendStatus(200);
+  });
 };
 
 function uploadFromStream(s3) {
@@ -31,14 +35,14 @@ function uploadFromStream(s3) {
 
   let params = {
     Body: pass,
-    Bucket: "annomate",
+    Bucket: BUCKET,
     Key: "exampleobject",
     Metadata: {
       "metadata1": "value1",
       "metadata2": "value2"
     }
   };
-  s3.putObject(params, function(err, data) {
+  s3.upload(params, function(err, data) {
     if (err)
       console.log(err, err.stack); // an error occurred
     else
@@ -52,6 +56,6 @@ router.get('/', function(req, res){
   res.send('hello world');
 });
 
-router.post('/image', binaryUploadHandler);
+router.post('/image', uploadHandler);
 
 module.exports = router;
