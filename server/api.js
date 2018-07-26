329a82d9d99a59b0eb209546335f7d094773524c
Richard Morton <richard.james.morton@gmail.com>
2018-07-26 21:59:07 +1000

    lodash!

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 const stream = require('stream');
+const _ = require('lodash');
 
 const router = express.Router();
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
