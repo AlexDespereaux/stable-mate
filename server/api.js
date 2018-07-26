236255f6afd98281c1e0abde618253857db9a6f2
Richard Morton <richard.james.morton@gmail.com>
2018-07-26 22:16:35 +1000

    basic binary upload

--- server/api.js
+++ server/api.js
@@ -6,44 +6,48 @@ const _ = require('lodash');
 const router = express.Router();
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const BUCKET = 'annomate';
+const TOKEN = '1F8065545D842E0098709630DBDBEB596D4D6194';
+
+let imageCounter = 0;
 
 let printRequestHeaders = function(req) {
-  console.log("\nReceived headers");
-  console.log("----------------");
+  console.log('\nReceived headers');
+  console.log('----------------');
 
   for (let key in req.headers) {
-    console.log(key + ": " + req.headers[key]);
+    console.log(key + ': ' + req.headers[key]);
   }
 
-  console.log("");
+  console.log('');
 };
 
 let uploadHandler = function(req, res) {
-  //TODO: mask for token, incorrect upload types
-
-  console.log("\n\nBinary Upload Request from: " + req.ip);
+  console.log('\n\nUpload Request from: ' + req.ip);
   printRequestHeaders(req);
-
-
-  let filename = req.headers["file-name"];
-  console.log("Started binary upload of: " + filename);
-  req.pipe(uploadFromStream(s3));
-  req.on('end', function() {
-    res.sendStatus(200);
-  });
+  if (!req.headers['token'] || req.headers['token'] !== TOKEN) {
+    res.sendStatus(401);
+  } else {
+    let fileExt = '.unk';
+    if (req.headers['Content-Type'] === 'image/png')
+      fileExt = '.png';
+    if (req.headers['Content-Type'] === 'image/jpeg')
+      fileExt = '.jpg';
+    console.log('Started upload from: ' + req.ip);
+    req.pipe(uploadFromStream(s3, fileExt));
+    req.on('end', function () {
+      res.sendStatus(200);
+    });
+  }
 };
 
-function uploadFromStream(s3) {
+let uploadFromStream = function(s3, fileExt) {
   let pass = new stream.PassThrough();
 
   let params = {
     Body: pass,
     Bucket: BUCKET,
-    Key: "exampleobject",
-    Metadata: {
-      "metadata1": "value1",
-      "metadata2": "value2"
-    }
+    Key: 'image' + _.padStart(imageCounter++, 6, '0') + fileExt
+    // Metadata: { "metadata1": "value1", "metadata2": "value2" }
   };
   s3.upload(params, function(err, data) {
     if (err)
@@ -53,7 +57,7 @@ function uploadFromStream(s3) {
   });
 
   return pass;
-}
+};
 
 router.get('/', function(req, res){
   res.send('hello world');
