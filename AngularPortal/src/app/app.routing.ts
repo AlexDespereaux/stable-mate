65803959803e38439698416b26f94b773289df16
anmolwalia <anmol@aerion.com.au>
2018-08-08 10:28:41 +1000

    imported component

--- AngularPortal/src/app/app.routing.ts
+++ AngularPortal/src/app/app.routing.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DisplayClassComponent } from './display-class/display-class.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
