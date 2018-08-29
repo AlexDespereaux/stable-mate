0d69effcedc5721cbeddb8474b31fce79459f319
Richard Morton <richard.james.morton@gmail.com>
2018-08-29 20:55:16 +1000

    create user function and remove username from user db, add admin bool to user db

--- server/src/database.js
+++ server/src/database.js
@@ -21,7 +21,7 @@ let connection = function() {
   });
 };
 
-const COLUMN_VALUES = ['filename', 'description', 'notes', 'datetime', 'latitude', 'longitude',
+const IMAGE_COLUMN_VALUES = ['filename', 'description', 'notes', 'datetime', 'latitude', 'longitude',
   'dFov', 'ppm', 'userId'];
 
 const DUMMY_USER_VALUE = {'userId': 1};
@@ -29,13 +29,29 @@ const DUMMY_USER_VALUE = {'userId': 1};
 exports.insertImageData = function(data, callback){
   connection().then(connection => {
     let flattenedData = _.merge({}, data, data.location, DUMMY_USER_VALUE);
-    let insertVals = _.pick(flattenedData, COLUMN_VALUES);
+    let insertVals = _.pick(flattenedData, IMAGE_COLUMN_VALUES);
     let sql = mysql.format('INSERT INTO images SET ?;', insertVals);
-    connection.query(sql, function (error, results, fields) {
+    connection.query(sql, function (error, results) {
       if (error) throw error;
       console.log(results);
       connection.end();
       callback({'imageId':results.insertId});
+    })
+  });
+};
+
+const USER_COLUMN_VALUES = ['password','email'];
+
+exports.createUser = function(data) {
+  return new Promise((resolve, reject) => {
+    connection().then(connection => {
+      let insertVals = _.pick(data, USER_COLUMN_VALUES);
+      let sql = mysql.format('INSERT INTO users SET ?;', insertVals);
+      connection.query(sql, function(err, result) {
+        if (error) reject(error);
+        connection.end();
+        resolve({'userId': result.insertId});
+      })
     })
   });
 };
