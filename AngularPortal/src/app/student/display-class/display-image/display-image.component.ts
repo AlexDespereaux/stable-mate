168f0339428edcbae550705cd5cb669c6bab9e28
anmolwalia <anmol@aerion.com.au>
2018-09-19 13:41:36 +1000

    Saving images and text file in proper order.

--- AngularPortal/src/app/student/display-class/display-image/display-image.component.ts
+++ AngularPortal/src/app/student/display-class/display-image/display-image.component.ts
@@ -83,46 +83,51 @@ export class DisplayImageComponent implements OnInit {
 
     // var button = document.getElementById("btn-Convert-Html2Image");
 
-    // var data = document.getElementById('image');
-    // html2canvas(data).then(canvas => {
-    //   // Few necessary setting options  
-    //   var imgWidth = 208;
-    //   var pageHeight = 295;
-    //   var imgHeight = canvas.height * imgWidth / canvas.width;
-    //   var heightLeft = imgHeight;
-    //   const contentDataURL = canvas.toDataURL('image/png');
-    //   $(button).attr('download', 'page.jpg');
-    //   let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
-    //   var position = 0;
-    //   pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-    //   // pdf.save('image.pdf'); // Generated PDF   
-    // });
+    var data = document.getElementById('image');
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options  
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+      const contentDataURL = canvas.toDataURL('image/png');
+      canvas.toBlob(function (blob) {
+        saveAs(blob, "image.png");
+      });
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      // pdf.save('image.pdf'); // Generated PDF   
+    });
 
 
 
 
-    // var data = document.getElementById('legend');
-    // html2canvas(data).then(canvas => {
-    //   // Few necessary setting options  
-    //   var imgWidth = 208;
-    //   var pageHeight = 295;
-    //   var imgHeight = canvas.height * imgWidth / canvas.width;
-    //   var heightLeft = imgHeight;
+    var data = document.getElementById('legend');
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options  
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
 
-    //   const contentDataURL = canvas.toDataURL('image/png');
-    //   contentDataURL.attr('download', 'legend.png');
+      const contentDataURL = canvas.toDataURL('image/png');
+      contentDataURL.attr('download', 'legend.png');
 
 
-    //   let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
-    //   var position = 0;
-    //   pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-    //   pdf.save('legened.pdf'); // Generated PDF   
-    // });
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      canvas.toBlob(function (blob) {
+        saveAs(blob, "legend.png");
+      });
+      // pdf.save('legened.pdf'); // Generated PDF   
+    });
 
-    const description  = document.getElementById("descriptions").textContent;
+    const description = document.getElementById("descriptions").textContent;
     var file = new File([description], "description.txt", { type: "text/plain;charset=utf-8" });
     saveAs(file);
-    
+
 
   }
 
