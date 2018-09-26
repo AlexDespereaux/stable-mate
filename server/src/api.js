8e230cc6aa50cee757abba9339b8ba644f9b4113
Richard Morton <richard.james.morton@gmail.com>
2018-09-26 17:35:23 +1000

    refactor all middleware functions to handlers.js

--- server/src/api.js
+++ server/src/api.js
@@ -1,131 +1,22 @@
 const express = require('express');
-const AWS = require('aws-sdk');
-const stream = require('stream');
-const _ = require('lodash');
-const auth = require('basic-auth');
-
-const db = require('./database');
+const handlers = require('./handlers');
 const router = express.Router();
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-const BUCKET = 'annomate';
-
-let imageCounter = 0;
-
-let printRequestHeaders = function (req) {
-  console.log('\nReceived headers');
-  console.log(Date());
-  console.log('----------------');
-  for (let key in req.headers) {
-    console.log(key + ': ' + req.headers[key]);
-  }
-  console.log('');
-};
-
-let uploadFromStream = function (s3) {
-  let pass = new stream.PassThrough();
-  let params = {
-    Body: pass,
-    Bucket: BUCKET,
-    Key: 'image' + _.padStart(imageCounter++, 6, '0') + '.png'
-  };
-  s3.upload(params, function (err) {
-    if (err)
-      console.log(err, err.stack); // an error occurred
-  });
-  return pass;
-};
-
-let imageHandler = function (req, res, next) {
-  printRequestHeaders(req);
-  if (req.get('Content-Type') === 'image/png') {
-    console.log('Started upload from: ' + req.ip);
-    req.pipe(uploadFromStream(s3));
-    req.on('end', function () {
-      res.status(200).send('Image upload complete!');
-    });
-  } else {
-    next();
-  }
-};
-
-let dataHandler = function (req, res) {
-  let status = 400;
-  let result = "";
-  const REQUIRED_KEYS = ['filename', 'description', 'notes', 'datetime', 'location', 'dFov', 'ppm', 'legend'];
-  if (_.every(REQUIRED_KEYS, (key) => req.body[key])) {
-    db.getUserId(req)
-      .then(userId => {
-        let data = _.merge(req.body, userId);
-        return db.insertImageData(data)
-      })
-      .then(imageId => {
-        status = 201;
-        result = imageId;
-      })
-      .catch(error => {
-        status = 500;
-        result = error;
-      })
-      .then(() => {
-        res.status(status).send(result);
-      });
-  } else {
-    res.status(400).send('Missing information or malformed json');
-  }
-};
-
-let authorise = function (req, res, next) {
-  let credentials = auth(req);
-  if (credentials && credentials['name'] && credentials['pass']) {
-    db.validateUser(credentials).then(() => {
-      next();
-    }).catch((error) => {
-      res.status(401).header('WWW-Authenticate', 'Basic').send(error);
-      next('router');
-    });
-  } else {
-    res.status(401).header('WWW-Authenticate', 'Basic').send('Access denied');
-    next('router');
-  }
-};
-
-let userType = function (req, res) {
-  let status = 400;
-  let result = '';
-  db.getUserType(req)
-    .then(userType => {
-      status = 200;
-      result = {'userType': userType}
-    })
-    .catch(error => {
-      status = 500;
-      result = error
-    })
-    .then(() => {
-      res.status(status).send(result)
-    });
-};
 
 router.post('/user', express.json(), function (req, res) {
 });
 
-router.use(authorise);
+router.use(handlers.printRequest);
 
-router.get('/user', userType);
+router.use(handlers.authorise);
 
-router.get('/image/:imageId.png', function (req, res) {
-  let s3params = {
-    Bucket: BUCKET,
-    Key: 'image' + _.padStart(req.params.imageId, 6, '0') + '.png'
-  };
-  s3.getObject(s3params, function (err, data) {
-    if (err) res.status(500).send(err + " " + err.stack);
-    else res.status(200).set('Content-Type', 'image/png').send(data.Body);
-  });
+router.get('/user', handlers.userType);
+
+router.get('/image/:imageId.png', handlers.imageDownload);
+
+router.get('/image', function (req, res) {
+
 });
 
-
-router.post('/image', imageHandler, express.json(), dataHandler);
+router.post('/image', handlers.imageUpload, express.json(), handlers.dataHandler);
 
 module.exports = router;
---
+++ server/src/handlers.js
@@ -0,0 +1,115 @@
+const AWS = require('aws-sdk');
+const stream = require('stream');
+const _ = require('lodash');
+const auth = require('basic-auth');
+
+const db = require('./database');
+
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+const BUCKET = 'annomate';
+
+exports.printRequest = function (req) {
+  console.log('\nReceived headers');
+  console.log(Date());
+  console.log('----------------');
+  for (let key in req.headers) {
+    console.log(key + ': ' + req.headers[key]);
+  }
+  console.log('');
+};
+
+let uploadFromStream = function (s3) {
+  let pass = new stream.PassThrough();
+  let params = {
+    Body: pass,
+    Bucket: BUCKET,
+    Key: 'image' + _.padStart(imageCounter++, 6, '0') + '.png'
+  };
+  s3.upload(params, function (err) {
+    if (err)
+      console.log(err, err.stack); // an error occurred
+  });
+  return pass;
+};
+
+exports.imageUpload = function (req, res, next) {
+  if (req.get('Content-Type') === 'image/png') {
+    console.log('Started upload from: ' + req.ip);
+    req.pipe(uploadFromStream(s3));
+    req.on('end', function () {
+      res.status(200).send('Image upload complete!');
+    });
+  } else {
+    next();
+  }
+};
+
+exports.dataHandler = function (req, res) {
+  let status = 400;
+  let result = "";
+  const REQUIRED_KEYS = ['filename', 'description', 'notes', 'datetime', 'location', 'dFov', 'ppm', 'legend'];
+  if (_.every(REQUIRED_KEYS, (key) => req.body[key])) {
+    db.getUserId(req)
+      .then(userId => {
+        let data = _.merge(req.body, userId);
+        return db.insertImageData(data)
+      })
+      .then(imageId => {
+        status = 201;
+        result = imageId;
+      })
+      .catch(error => {
+        status = 500;
+        result = error;
+      })
+      .then(() => {
+        res.status(status).send(result);
+      });
+  } else {
+    res.status(400).send('Missing information or malformed json');
+  }
+};
+
+exports.authorise = function (req, res, next) {
+  let credentials = auth(req);
+  if (credentials && credentials['name'] && credentials['pass']) {
+    db.validateUser(credentials).then(() => {
+      next();
+    }).catch((error) => {
+      res.status(401).header('WWW-Authenticate', 'Basic').send(error);
+      next('router');
+    });
+  } else {
+    res.status(401).header('WWW-Authenticate', 'Basic').send('Access denied');
+    next('router');
+  }
+};
+
+exports.userType = function (req, res) {
+  let status = 400;
+  let result = '';
+  db.getUserType(req)
+    .then(userType => {
+      status = 200;
+      result = {'userType': userType}
+    })
+    .catch(error => {
+      status = 500;
+      result = error
+    })
+    .then(() => {
+      res.status(status).send(result)
+    });
+};
+
+exports.imageDownload = function (req, res) {
+  let s3params = {
+    Bucket: BUCKET,
+    Key: 'image' + _.padStart(req.params.imageId, 6, '0') + '.png'
+  };
+  s3.getObject(s3params, function (err, data) {
+    if (err) res.status(500).send(err + " " + err.stack);
+    else res.status(200).set('Content-Type', 'image/png').send(data.Body);
+  });
+};
