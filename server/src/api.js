4ea4bd596e3059d59c7b2a4c32dccdec02ab0682
Richard Morton <richard.james.morton@gmail.com>
2018-09-26 12:26:22 +1000

    indentation and refactor datahandler

--- server/src/api.js
+++ server/src/api.js
@@ -12,7 +12,7 @@ const BUCKET = 'annomate';
 
 let imageCounter = 0;
 
-let printRequestHeaders = function(req) {
+let printRequestHeaders = function (req) {
   console.log('\nReceived headers');
   console.log(Date());
   console.log('----------------');
@@ -22,7 +22,7 @@ let printRequestHeaders = function(req) {
   console.log('');
 };
 
-let uploadFromStream = function(s3) {
+let uploadFromStream = function (s3) {
   let pass = new stream.PassThrough();
 
   let params = {
@@ -30,64 +30,87 @@ let uploadFromStream = function(s3) {
     Bucket: BUCKET,
     Key: 'image' + _.padStart(imageCounter++, 6, '0') + '.png'
   };
-  s3.upload(params, function(err) {
+  s3.upload(params, function (err) {
     if (err)
       console.log(err, err.stack); // an error occurred
-    });
+  });
   return pass;
 };
 
-let imageHandler = function(req, res, next) {
+let imageHandler = function (req, res, next) {
   printRequestHeaders(req);
   if (req.get('Content-Type') === 'image/png') {
     console.log('Started upload from: ' + req.ip);
     req.pipe(uploadFromStream(s3));
-    req.on('end', function() { res.status(200).send('Image upload complete!'); });
+    req.on('end', function () {
+      res.status(200).send('Image upload complete!');
+    });
   } else {
     next();
   }
 };
 
-let dataHandler = function(req, res) {
-  db.getUserId(req).then(userId => {
-    const REQUIRED_KEYS = ['filename', 'description', 'notes', 'datetime', 'location', 'dFov', 'ppm', 'legend'];
-    if (_.every(REQUIRED_KEYS, (key) => req.body[key])) {
-      let data = _.merge(req.body, userId);
-      db.insertImageData(data).then(result => {
-       res.status(200).send(result)
-      }).catch(error => res.status(500).send(error));
-    } else {
-      res.status(400).send('Missing information or malformed json')
-    }
-  }).catch(error => res.status(500).send(error));
+let dataHandler = function (req, res) {
+  let status = 400;
+  let result = "";
+  db.getUserId(req)
+    .then(userId => {
+      const REQUIRED_KEYS = ['filename', 'description', 'notes', 'datetime', 'location', 'dFov', 'ppm', 'legend'];
+      if (_.every(REQUIRED_KEYS, (key) => req.body[key])) {
+        let data = _.merge(req.body, userId);
+        db.insertImageData(data)
+          .then(imageId => {
+            status = 200;
+            result = imageId;
+          })
+          .catch(error => {
+            status = 500;
+            result = error;
+          });
+      } else {
+        status = (400);
+        result = 'Missing information or malformed json';
+      }
+    })
+    .catch(error => {
+      status = 500;
+      result = error
+    });
+  res.status(status).send(result);
 };
 
-let authorise = function(req, res, next) {
+let authorise = function (req, res, next) {
   let credentials = auth(req);
   if (credentials && credentials['name'] && credentials['pass']) {
     db.validateUser(credentials).then(() => {
       next();
-    }).catch((error) => { console.log(error)});
+    }).catch((error) => {
+      res.status(401).header('WWW-Authenticate', 'Basic').send(error);
+      next('router');
+    });
+  } else {
+    res.status(401).header('WWW-Authenticate', 'Basic').send('Access denied');
+    next('router');
   }
-  res.status(401).header('WWW-Authenticate', 'Basic').send('Access denied');
 };
 
-router.post('/user', express.json(), function(req, res) {});
+router.post('/user', express.json(), function (req, res) {
+});
 
 router.use(authorise);
 
-router.get('/user', function(req, res) {
+router.get('/user', function (req, res) {
   res.status(200).send('Authenticated')
 });
 
-router.get('/image/:imageId.png', function(req, res) {
+router.get('/image/:imageId.png', function (req, res) {
   let s3params = {
     Bucket: BUCKET,
     Key: 'image' + _.padStart(req.params.imageId, 6, '0') + '.png'
   };
-  s3.getObject(s3params, function(err, data) {
+  s3.getObject(s3params, function (err, data) {
     if (err) res.status(500).send(err + " " + err.stack);
-    else     res.status(200).set('Content-Type','image/png').send(data.Body);
+    else res.status(200).set('Content-Type', 'image/png').send(data.Body);
   });
 });
 
