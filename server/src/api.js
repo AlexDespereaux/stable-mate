54747b129268d20fa8c436abe1a9cf30901a782a
Richard Morton <richard.james.morton@gmail.com>
2018-10-03 14:36:12 +1000

    add cors package for anmols local tests

--- server/src/api.js
+++ server/src/api.js
@@ -2,19 +2,16 @@ const express = require('express');
 const handlers = require('./handlers');
 const router = express.Router();
 const db = require('./database');
+const cors = require('cors');
 
 router.post('/user', express.json(), function (req, res) {
 });
 
 router.use(handlers.printRequest);
 
-router.use(handlers.authorise);
+router.use(cors());
 
-router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+router.use(handlers.authorise);
 
 router.get('/user', handlers.userType);
 
