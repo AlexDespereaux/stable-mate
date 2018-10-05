8b72784c0b685e8b1bb69b936f3abae6556e32f0
Richard Morton <richard.james.morton@gmail.com>
2018-10-05 19:05:50 +1000

    user creation function

--- server/src/api.js
+++ server/src/api.js
@@ -4,9 +4,6 @@ const router = express.Router();
 const db = require('./database');
 const cors = require('cors');
 
-router.post('/user', express.json(), function (req, res) {
-});
-
 router.use(handlers.printRequest);
 
 router.use(cors());
@@ -15,12 +12,26 @@ router.use(handlers.authorise);
 
 router.get('/user', handlers.userType);
 
+let userAdmin = function (userType) {
+  return new Promise((resolve) => resolve(userType === 'admin'));
+};
+
+router.post('/user', express.json(), function (req, res) {
+  db.getUserType(req)
+    .then(userType => { return userAdmin(userType) })
+    .then(userAdmin => { if (userAdmin) { return db.createUser(req.body) } else { throw 'Not an admin' }})
+    .then(userId => res.status(200).send(userId))
+    .catch(error => res.status(401).send(error));
+});
+
+router.post('/image/:type/:imageId', handlers.imageUpload);
+
 router.get('/image/:imageId', function(req, res) {res.status(200).send(req.body)});
 
 router.get('/image/:imageId.png', handlers.imageDownload);
 
 router.get('/image', handlers.imageList);
 
-router.post('/image', handlers.imageUpload, express.json(), handlers.dataHandler);
+router.post('/image', express.json(), handlers.dataHandler);
 
 module.exports = router;
