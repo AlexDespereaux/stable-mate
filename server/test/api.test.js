0889581630dfd51006efb5226e300a82d7ee4992
Richard Morton <richard.james.morton@gmail.com>
2018-10-14 15:31:07 +1100

    tidied up api tests

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -2,20 +2,13 @@
 const request = require('supertest');
 const express = require('express');
 const api = require('../src/api');
+const sinon = require('sinon');
 
 describe('api', function() {
   let app = express();
   let server;
-  let sinonSandbox;
-  const TOKEN = '1F8065545D842E0098709630DBDBEB596D4D6194';
-
 
   before(function(done) {
-    // sinonSandbox = sinon.sandbox.create();
-    // sinonSandbox.stub(s3, 'upload')
-    //   .returns({
-    //
-    //   });
     app.use(api);
     server = app.listen(function(err) {
       if (err) { return done(err); }
@@ -25,28 +18,16 @@ describe('api', function() {
 
   after(function(){
     server.close();
-    // sinonSandbox.restore();
   });
 
-  it('POST /image blank/no token', function(done){
+  afterEach(() => {
+    // Restore the default sandbox here
+    sinon.restore();
+  });
+
+  it('post /image blank/no token', function(done){
     request(app)
       .post('/image')
       .expect(401, done);
   });
-
-  it('POST /image blank/with token', function(done){
-    request(app)
-      .post('/image')
-      .set('token', TOKEN)
-      .expect(400, done);
-  });
-
-  // it('POST /image png/with token', function(done){
-  //   request(app)
-  //     .post('/image')
-  //     .set('token', TOKEN)
-  //     .set('Content-Type', 'image/png')
-  //     .expect(200, done);
-  // });
-
 });
