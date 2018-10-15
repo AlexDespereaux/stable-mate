b0be8d78c3a87d68e5202c33ad2af4b265b6be81
Marita Fitzgerald <18976492@students.latrobe.edu.au>
2018-10-16 04:33:27 +1100

    Added, about, help & removeed filter

--- Android/JaramBuild/app/src/main/java/com/jaram/jarambuild/AboutUsActivity.java
+++ Android/JaramBuild/app/src/main/java/com/jaram/jarambuild/AboutUsActivity.java
@@ -1,14 +1,23 @@
 package com.jaram.jarambuild;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.jaram.jarambuild.utils.TinyDB;
 
 public class AboutUsActivity extends AppCompatActivity
 {
+    String TAG = "About";
+    //user
+    TinyDB tinydb;
+    String loggedInUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,12 +38,52 @@ public class AboutUsActivity extends AppCompatActivity
                 }
             });
         }
+
+        tinydb = new TinyDB(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.bar_menu, menu);
+        return true;
+    }
+
+    //custom menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.settingsMenuBtn)
+        {
+            Log.d(TAG, "Settings Btn Clicked");
+            //Go to settings activity
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+        } else if (item.getItemId() == R.id.helpMenuBtn)
+        {
+            Log.d(TAG, "Help Btn Clicked");
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://docs.google.com/document/d/1CFCF-80XOzv55uB1acoBkKkKK8FgZzDq0q24luXXdzI/edit?usp=sharing"));
+            startActivity(browserIntent);
+
+        } else if (item.getItemId() == R.id.logoutMenuBtn)
+        {
+            Log.d(TAG, "Logout Btn Clicked");
+            //set logged in user to null
+            tinydb.putString("loggedInAccount", "");
+            //return to Login Page
+            Intent settingsIntent = new Intent(this, MainActivity.class);
+            startActivity(settingsIntent);
+            finish();
+        }
+        else if (item.getItemId() == R.id.aboutBtn)
+        {
+            Log.d(TAG, "About Btn Clicked");
+        }
+        else
+        {
+            return super.onOptionsItemSelected(item);
+        }
         return true;
     }
 
