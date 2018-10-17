7a1733428d99c00a29c690d99d153173d8580489
Anmol <anmol@aerion.com.au>
2018-10-17 16:11:46 +1100

    Uncomment bug fix

--- AngularPortal/src/app/student/display-class/display-image/display-image.component.ts
+++ AngularPortal/src/app/student/display-class/display-image/display-image.component.ts
@@ -113,21 +113,21 @@ export class DisplayImageComponent implements OnInit {
 
     // var button = document.getElementById("btn-Convert-Html2Image");
 
-    // var data = document.getElementById('image');
-    // html2canvas(data).then(canvas => {
-    //   // Few necessary setting options  
-    //   var imgWidth = 208;
-    //   var pageHeight = 295;
-    //   var imgHeight = canvas.height * imgWidth / canvas.width;
-    //   var heightLeft = imgHeight;
-    //   const contentDataURL = canvas.toDataURL('image/png');
-    //   canvas.toBlob(function (blob) {
-    //     saveAs(blob, "image.png");
-    //   });
-    //   let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
-    //   var position = 0;
-    //   pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-    // });
+    var data = document.getElementById('image');
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options  
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+      const contentDataURL = canvas.toDataURL('image/png');
+      canvas.toBlob(function (blob) {
+        saveAs(blob, "image.png");
+      });
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+    });
 
 
 
@@ -152,9 +152,9 @@ export class DisplayImageComponent implements OnInit {
       });
     });
 
-    // const description = document.getElementById("descriptions").textContent;
-    // var file = new File([description], "description.txt", { type: "text/plain;charset=utf-8" });
-    // saveAs(file);
+    const description = document.getElementById("descriptions").textContent;
+    var file = new File([description], "description.txt", { type: "text/plain;charset=utf-8" });
+    saveAs(file);
   }
 
   downlaodDescription() {
